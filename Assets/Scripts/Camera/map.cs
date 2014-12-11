78f7a4cb6c15f65c1f47c409e6b8415ec12213ac
Guillermo <thecrow88@gmail.com>
2014-12-11 21:16:23 +0100

    I have continued the mini-map (the units are painted on it)

--- Assets/Scripts/Camera/map.cs
+++ Assets/Scripts/Camera/map.cs
@@ -10,16 +10,22 @@ public class Map : MonoBehaviour
 {
     private static Material lineMaterial;
 
-    public GameObject panelMap;
+    //public GameObject panelMap;
     public LayerMask layer;
     public GameObject plane;
 
     private int heightScreen, widthScreen;
-    private float divHeight, divWidth;
+    private float divHeightMap, divWidthMap, divHeightMinMap, divWidthMinMap;
     private float hMinMap, wMinMap, hMap, wMap;
     private float hRel, wRel;
     private Vector2 p1, p2, p3, p4;
     private Vector3 bounds;
+    private Texture2D textBackgroundMinimap;
+    private Rect[][] minimapMatrix;
+    private Texture2D textUnit;
+    private Rect[] units;
+
+    //------------------------------------------
 
     static void CreateLineMaterial()
     {
@@ -39,27 +45,59 @@ public class Map : MonoBehaviour
 
     void Start()
     {
+        // bounds of terrain
         bounds = plane.GetComponent<MeshFilter>().mesh.bounds.size;
-
+        // attributes of map and minimap
         heightScreen = Screen.height;
         widthScreen = Screen.width;
-        divHeight = heightScreen / 100.0f;
-        divWidth = widthScreen / 100.0f;
-        hMinMap = 29 * divHeight;
-        wMinMap = 19 * divWidth;
+        divHeightMap = heightScreen / 100.0f;
+        divWidthMap = widthScreen / 100.0f;
+        hMinMap = 30 * divHeightMap;
+        wMinMap = 20 * divWidthMap;
+        divHeightMinMap = hMinMap / 100.0f;
+        divWidthMinMap = wMinMap / 100.0f;
         hMap = bounds.z * 2;
         wMap = bounds.x * 2;
         hRel = hMinMap / hMap;
         wRel = wMinMap / wMap;
+        // background texture for minimap
+        textBackgroundMinimap = new Texture2D(2, 2, TextureFormat.ARGB32, false);
+        textBackgroundMinimap.SetPixel(0, 0, Color.black);
+        textBackgroundMinimap.SetPixel(1, 0, Color.black);
+        textBackgroundMinimap.SetPixel(0, 1, Color.black);
+        textBackgroundMinimap.SetPixel(1, 1, Color.black);
+        textBackgroundMinimap.Apply();
+        // minimap matrix
+        minimapMatrix = new Rect[100][];
+        for (int i = 0; i < 100; i++)
+        {
+            minimapMatrix[i] = new Rect[100];
+            for (int j = 0; j < 100; j++)
+            {
+                Vector2 pos = PointInvertedIntoMinimap(new Vector2(i * divWidthMinMap, j * divHeightMinMap));
+                //minimapMatrix[i][j] = new Rect(i * divWidthMinMap + 0.01f * widthScreen, (heightScreen - 0.01f * heightScreen - (100 - j) * divHeightMinMap), divWidthMinMap, divHeightMinMap);
+                minimapMatrix[i][j] = new Rect(pos.x, pos.y, divWidthMinMap, divHeightMinMap);
+            }
+        }
+        // texture units
+        textUnit = new Texture2D(2, 2, TextureFormat.ARGB32, false);
+        textUnit.SetPixel(0, 0, Color.green);
+        textUnit.SetPixel(1, 0, Color.green);
+        textUnit.SetPixel(0, 1, Color.green);
+        textUnit.SetPixel(1, 1, Color.green);
+        textUnit.Apply();
+        // units
+        units = new Rect[InformationUnits.units.Count];
 
-        panelMap.SetActive(true);
-        RectTransform rt = panelMap.GetComponent<RectTransform>();
-        rt.anchorMin = new Vector2(0.01f, 0.01f);
-        rt.anchorMax = new Vector2(0.2f, 0.3f);
+        //panelMap.SetActive(true);
+        //RectTransform rt = panelMap.GetComponent<RectTransform>();
+        //rt.anchorMin = new Vector2(0.01f, 0.01f);
+        //rt.anchorMax = new Vector2(0.2f, 0.3f);
     }
 
     void Update()
     {
+        // camera projection
         Ray ray1, ray2, ray3, ray4;
         ray1 = Camera.main.ScreenPointToRay(Vector3.zero);
         ray2 = Camera.main.ScreenPointToRay(new Vector3(widthScreen, 0, 0));
@@ -70,27 +108,54 @@ public class Map : MonoBehaviour
         if (Physics.Raycast(ray1, out hit1, 250.0f, layer) && hit1.transform.CompareTag("Terrain"))
         {
             Debug.DrawLine(ray1.origin, hit1.point, Color.red);
-            p1 = new Vector2((hit1.point.x + bounds.x)*wRel + divWidth, (hit1.point.z + bounds.z)*hRel + divHeight);
+            p1 = Worldmap2Minimap(hit1.point);
 
         }
         if (Physics.Raycast(ray2, out hit2, 250.0f, layer) && hit2.transform.CompareTag("Terrain"))
         {
             Debug.DrawLine(ray2.origin, hit2.point, Color.red);
-            p2 = new Vector2((hit2.point.x + bounds.x) * wRel + divWidth, (hit2.point.z + bounds.z) * hRel + divHeight);
+            p2 = Worldmap2Minimap(hit2.point);
         }
         if (Physics.Raycast(ray3, out hit3, 250.0f, layer) && hit3.transform.CompareTag("Terrain"))
         {
             Debug.DrawLine(ray3.origin, hit3.point, Color.red);
-            p3 = new Vector2((hit3.point.x + bounds.x) * wRel + divWidth, (hit3.point.z + bounds.z) * hRel + divHeight);
+            p3 = Worldmap2Minimap(hit3.point);
         }
         if (Physics.Raycast(ray4, out hit4, 250.0f, layer) && hit4.transform.CompareTag("Terrain"))
         {
             Debug.DrawLine(ray4.origin, hit4.point, Color.red);
-            p4 = new Vector2((hit4.point.x + bounds.x) * wRel + divWidth, (hit4.point.z + bounds.z) * hRel + divHeight);
+            p4 = Worldmap2Minimap(hit4.point);
+        }
+        // units position
+        for (int i = 0; i < InformationUnits.units.Count; i++)
+        {
+            Vector2 pos = Worldmap2Minimap(InformationUnits.units[i].transform.position);
+            pos = PointSymmetricIntoMinimap(pos);
+            units[i] = new Rect(pos.x, pos.y, 2, 2);
         }
     }
 
-    void OnPostRender()
+    void OnGUI()
+    {
+        // minimap background
+        for (int i = 0; i < 100; i++)
+            for (int j = 0; j < 100; j++)
+            {
+                GUI.DrawTexture(minimapMatrix[i][j], textBackgroundMinimap);
+            }
+        // units
+        for (int i = 0; i < InformationUnits.units.Count; i++)
+        {
+            GUI.DrawTexture(units[i], textUnit);
+        }
+        // camera projection
+        GLPaint();
+    }
+
+    //----------------------------------
+
+    // Camera projection
+    private void GLPaint()
     {
         CreateLineMaterial();
         // set the current material
@@ -110,13 +175,23 @@ public class Map : MonoBehaviour
 
         GL.Vertex3(p4.x, p4.y, 0);
         GL.Vertex3(p1.x, p1.y, 0);
-
-        //GL.Color(Color.blue);
-        //GL.Vertex3(1, 1, 0);
-        //GL.Vertex3(1, 2, 0);
-        //GL.Vertex3(2, 1, 0);
-        //GL.Vertex3(2, 2, 0);
         GL.End();
         GL.PopMatrix();
+    }
+
+    // Transform a world point into a minimap point
+    private Vector2 Worldmap2Minimap(Vector3 worldPoint)
+    {
+        return new Vector2((worldPoint.x + bounds.x) * wRel + divWidthMap, (worldPoint.z + bounds.z) * hRel + divHeightMap);
+    }
+
+    private Vector2 PointInvertedIntoMinimap(Vector2 point)
+    {
+        return new Vector2(point.x + 0.01f * widthScreen, heightScreen - 0.01f * heightScreen - hMinMap + point.y);
+    }
+
+    private Vector2 PointSymmetricIntoMinimap(Vector2 point)
+    {
+        return new Vector2(point.x, heightScreen - point.y);
     }
 }
--- Assets/Scripts/Information/InformationUnits.cs
+++ Assets/Scripts/Information/InformationUnits.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class InformationUnits 
+public class InformationUnits : MonoBehaviour
 {
     public static List<GameObject> units = new List<GameObject>();
 }
